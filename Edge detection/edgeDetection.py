fb3907a5eaebfbc0cedea14145d5ac5e1e849f16
Yatheen07 <pravan.yatheen@gmail.com>
2018-06-07 12:36:14 +0530

    Comment lines added for Edge Detection in Image

--- Edge detection/edgeDetection.py
+++ Edge detection/edgeDetection.py
@@ -2,22 +2,45 @@
 """
 Created on Thu Jun  7 10:29:24 2018
 
-@author: kmy07
+@author: yatheen!
 """
+"""Step 1: Import the neccessary Packages """
+import cv2  #OpenCv to handle and process the images
+import numpy as np #Images are stores as numpy arrays
+import matplotlib.pyplot as plt # to plot the image we have to use matplotlib
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython import get_ipython
-get_ipython().run_line_magic('matplotlib', 'qt')
+#----------------------------------------------------------------------------------------------#
 
-print("All packages Imported succesfully!")
+""" To display the result in a seperate Screen, use the following code (Optional) """
+from IPython import get_ipython # the plot is shown as a seperate screen
+get_ipython().run_line_magic('matplotlib', 'qt') # use Ipython package to make the run_line_magic function 
 
-image = cv2.imread(r"E:\MRV Project\Project 1\vp.jpg",0)
+print("All packages Imported succesfully!") #jus a check
 
-edges = cv2.Canny(image , 500,200)
+#----------------------------------------------------------------------------------------------#
 
-plt.subplot(121)
+"""Step 3: Read the image input """
+
+image = cv2.imread(r"E:\Image processing\Edge detection\vp.jpg",0) # get the image as input
+
+"""
+The second argument in the image determines how the input is readed
+0 ---> the image is read as grayscale image
+1 ---> the image is read as color image """
+
+#image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) if color image is read as input , change it to grayscale
+
+#----------------------------------------------------------------------------------------------#
+
+"""Step 4 : Detect the egdes using Canny Shape Descriptor """
+
+edges = cv2.Canny(image , 300,200) #Canny shape descriptor is used to identify the edges.
+
+#----------------------------------------------------------------------------------------------#
+
+"""Step 5: Plot the result """
+
+plt.subplot(121) #121 is the index of the plot of the original image 
 plt.imshow(image,cmap = 'gray')
 plt.title('Original Image')
 plt.xticks([])
@@ -26,7 +49,9 @@ plt.yticks([])
 plt.subplot(122)
 plt.imshow(edges ,cmap = 'gray')
 plt.title('Image with edges detected')
-plt.xticks([])
-plt.yticks([])
+plt.xticks([]) #Get or set the x-limits of the current tick locations and labels.
+plt.yticks([]) #Get or set the y-limits of the current tick locations and labels.
 
-plt.show()
+plt.show() # Display the plot
+
+#----------------------------------------------------------------------------------------------#
