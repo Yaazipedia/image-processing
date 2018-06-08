10cf029c0ba9a0aec34e8c62fbdc8884c6f607eb
Yatheen07 <pravan.yatheen@gmail.com>
2018-06-08 22:51:46 +0530

    Image brightness code updated

---
+++ openCv Basics/imageBrightness.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 21:24:32 2018
+
+@author: yatheen!
+"""
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread(r'E:\Programming!\Image processing\images\2.jpg')
+
+brightnessMatrix = np.ones(image.shape , dtype = "uint8" ) * 75
+
+"""To increase the brightness add the brightness matrix"""
+alteredImage = cv2.add(image , brightnessMatrix)
+cv2.imshow("Image Brighteness adjusted" , alteredImage)
+
+"""To decrease the brightness subtract the brightness matrix"""
+alteredImage = cv2.subtract(image , brightnessMatrix)
+cv2.imshow("Image Brighteness adjusted" , alteredImage)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
