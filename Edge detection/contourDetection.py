fbef037e5198ab0f71ad3c15b382c74b6a4034d1
Yatheen07 <pravan.yatheen@gmail.com>
2018-06-09 16:28:16 +0530

    Contour detection completed

---
+++ Edge detection/contourDetection.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 15:03:36 2018
+
+@author: yatheen!
+"""
+
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import time
+
+image =cv2.imread(r"E:\Programming!\Image processing\images\8.jpg",0)
+
+ret , threshold =cv2.threshold(image , 127 ,255 , 0)
+
+alteredImage, contours, hierarchy = cv2.findContours(threshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.imshow("Original Image" , image)
+path = r"E:/Programming!/Image processing/images/Blades Images/"
+
+image2 = cv2.drawContours(image , contours , 1 , (0,255,0), 6)
+#cv2.imwrite(os.path.join(path+'Blade '+str(i)+".jpg"),image2)
+
+cv2.imshow("Altered Image" , image2)
+print("Area of the detected contour is : " , cv2.contourArea(contours[1]))
+print("Arc of detected contour is : " ,cv2.arcLength(contours[1],True))
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
